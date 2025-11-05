612e478588f9a44b654afb5bb399a62f2f54938d
holcombet <holcombe.t.23@gmail.com>
2025-11-05 09:41:22 -0800

    monotonicity proof

--- FrameTheory/FrameProofs.lean
+++ FrameTheory/FrameProofs.lean
@@ -7,6 +7,8 @@ import Mathlib.CategoryTheory.Functor.Basic
 
 open CategoryTheory Order Frm Set
 
+universe u
+
 -- #check CategoryTheory.Mono.right_cancellation
 --
 -- Frm mono-morphism
@@ -14,11 +16,23 @@ open CategoryTheory Order Frm Set
 /-
 playing around with Frm definitions, instances, etc.
 -/
-variable (C : Frame X)
+
+variable (A B: Frm)
+variable (C F: Frame X)
 variable (D : Set α)
 
 
-#check C
+
+
+
+#check A.str.bot
+
+#check FrameHom A B
+
+variable (f : FrameHom A B)
+
+#check f.cancel_left
+
 
 /-
 theorem Frame.distJoin {α : Type u} [Frame α] (x y z : α) : x ⊔ (y ⊓ z) = (x ⊔ y) ⊓ (x ⊔ z)  := by
@@ -28,5 +42,48 @@ theorem Frame.sup_lub {α : Type u} [Frame α] (x : α) (S : Set α) (h : ∀ y 
   sorry
 -/
 
-theorem sup_lub {α : Type u}  [Frame α] (F :  α ) (S : Set α ) (h : ∀ y ∈ S, y ≤  F) : sSup S ≤ F := by
-  sorry
+/-
+instance hasForgetToLat : HasForget₂ Frm Lat where
+  forget₂.obj X := .of X
+  forget₂.map f := Lat.ofHom f.hom
+-/
+
+#check Set
+-- instance hasForgetToSet : HasForget₂ (Frm.{u}) (Set.{u}) where
+--   sorry
+
+-- instance hasForgetSetToSet : HasForget₂ Set Set where
+
+#check CategoryTheory.forget₂ Frm Lat
+
+#check CategoryTheory.forget Frm
+
+
+universe v
+
+
+/-
+* Proof for Monotonicity in frame homomorphisms
+-/
+
+variable {L M : Type*} [Frame L] [Frame M]
+
+-- `f : L → M` means: frame_hom L M
+variable (f : L → M)
+
+-- -- 1. A frame hom is monotone
+theorem FrameHom.monotone (f : FrameHom A B) :
+    Monotone f := by
+  intro x y h
+  -- we use x = x ⊓ y because x ≤ y in a frame
+  have hx : x = x ⊓ y := by
+    apply le_antisymm
+    · simpa [inf_of_le_right h] using (le_inf_iff.mpr ⟨le_rfl, h⟩)
+    · have : x ⊓ y ≤ x := inf_le_left; simp
+  -- apply f to both sides
+  have := congrArg f hx
+  -- simplify with map_inf
+  -- simpa using (this ▸ inf_le_right (f x) (f y))
+  simpa using this
+
+-- simpa checks if simplified result is the goal. if yes, closes goal
